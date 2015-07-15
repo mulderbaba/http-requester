c990e754fd6c28678675c38feb6cb43e16155747
Mert ÇALIŞKAN <mert@t2.com.tr>
2015-07-15 11:25:24 +0300

    removed unused stuff

--- src/main/java/tr/com/t2/labs/concurrent/ConcurrentQueue.java
+++ src/main/java/tr/com/t2/labs/concurrent/ConcurrentQueue.java
@@ -1,7 +1,6 @@
 package tr.com.t2.labs.concurrent;
 
 import java.util.ArrayDeque;
-import java.util.Collection;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -21,22 +20,6 @@ public class ConcurrentQueue<T> {
     public ConcurrentQueue(int capacity) {
         this.capacity = capacity;
         this.arrayDeque = new ArrayDeque<T>(capacity + 10);
-    }
-
-    public T poll() {
-        readLock.lock();
-        try {
-            while (true) {
-                if (size.get() == 0) {
-                    return null;
-                } else {
-                    size.decrementAndGet();
-                    return arrayDeque.removeFirst();
-                }
-            }
-        } finally {
-            readLock.unlock();
-        }
     }
 
     public T take() {
@@ -78,91 +61,13 @@ public class ConcurrentQueue<T> {
         }
     }
 
-    public boolean offer(T item, long delta) {
-        long start = System.currentTimeMillis();
-        while (true) {
-            if (size.get() >= capacity) {
-                long now = System.currentTimeMillis();
-                if (now - start > delta) {
-                    return false;
-                } else {
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } else {
-                addItem(item);
-                return true;
-            }
-        }
-    }
 
     protected void addItem(T item) {
         arrayDeque.addLast(item);
         size.incrementAndGet();
     }
 
-    public int drainTo(Collection<? super T> collection) {
-        readLock.lock();
-        try {
-            return drainTo(collection, size.get());
-        } finally {
-            readLock.unlock();
-        }
-    }
-
-    public int drainTo(Collection<? super T> collection, int maxItemCount) {
-        readLock.lock();
-        try {
-            int len = Math.min(size.get(), maxItemCount);
-            if (len > 0) {
-                for (int i = 0; i < len; i++) {
-                    collection.add(arrayDeque.removeFirst());
-                }
-                size.addAndGet(-len);
-            }
-            return len;
-        } finally {
-            readLock.unlock();
-        }
-    }
-
-    public void shutdown() {
-        shutdown.set(true);
-
-        while (true) {
-            if (size.get() > 0) {
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                try {
-                    shutdownCountDownLatch.await();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                return;
-            }
-        }
-    }
-
     public void onConsumerFinished() {
         shutdownCountDownLatch.countDown();
-    }
-
-    public int getSize() {
-        return size.get();
-    }
-
-    public boolean isEmpty() {
-        return getSize() == 0;
-    }
-
-    public int remainingCapacity() {
-        return capacity - getSize();
     }
 }
--- src/main/java/tr/com/t2/labs/concurrent/FastBlockingExecutor.java
+++ src/main/java/tr/com/t2/labs/concurrent/FastBlockingExecutor.java
@@ -5,15 +5,8 @@ import java.util.concurrent.Executor;
 
 public class FastBlockingExecutor implements Executor {
 
-	private static final int DEFAULT_THREAD_COUNT = 1;
-	private static final int DEFAULT_QUEUE_SIZE = 5;
-	
 	private ConcurrentQueue<Runnable> concurrentQueue;
 	
-	public FastBlockingExecutor(String name) {
-		this(name, DEFAULT_QUEUE_SIZE, DEFAULT_THREAD_COUNT);
-	}
-
 	public FastBlockingExecutor(String name, int queueSize, int threadCount) {
 		concurrentQueue = new ConcurrentQueue<Runnable>(queueSize);
 		new ConcurrentQueueConsumerThreadPool(name, concurrentQueue, threadCount);
